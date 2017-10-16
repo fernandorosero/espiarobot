ba321e03cabdbcfdfcf91e89e66fd8250eecba07
macf3rn4nd0 <macf3rn4nd0@localhost>
2017-10-16 02:48:38 +0200

    MAcbookPro

--- index.php
+++ index.php
@@ -14,10 +14,35 @@
         ?>
         
         <?php
-            include_once './section.php';
+        
+            if(isset($_POST['usuario'])){
+                    $_SESSION['usuario'] = $_POST['usuario'];
+                    //echo $url_Adminstracion;
+                    include_once './robocam.php';
+                }else {
+                    if(isset($_SESSION['usuario'])){
+                        //echo $url_Adminstracion;
+                        include_once './robocam.php';
+                      }else{
+                            //echo $url_Contacto;
+                          include_once './section.php';
+                      }
+                }
+            
+            if (isset($_SESSION['usuario'])){
+                echo '<p>Sesion iniciada:'.$_SESSION['usuario'].'';
+                echo '<p><a href="controller/FinSession.php">Cerrar sesion</a></p>';
+            }
+            else{
+                echo 'no session';
+            }
+        ?>
+        
+        <?php
+            
         ?>
         <?php
-        include_once './robocam.php';
+            
         ?>
         <?php
             include_once './footer.php';
