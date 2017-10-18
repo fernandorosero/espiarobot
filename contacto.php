54d37c298882607097a30823254c7a2f2a6bba66
fernandorosero <tatofarp@hotmail.com>
2017-10-18 02:14:40 +0200

    Lenovo-Casa

--- contacto.php
+++ contacto.php
@@ -1,7 +1,20 @@
-
+<?php
+    include_once './init.php';
+?>
 <div id="formContacto">
-    <p>Con</p>
-    <form>
-        
+    <form id="formularioDeContacto">
+        <h2>Contacto</h2>
+        <label>Nombre</label>
+        <input class="form-control" type="text" placeholder="Ingrese el nombre" name="nombre" required/>
+        <label>Teléfono</label>
+        <input class="form-control" type="number" placeholder="Ingese su teléfono" name="telefono"/>
+        <label>Email</label>
+        <input class="form-control" type="email" placeholder="Ingrese su email" name="email" required/>
+        <div class="form-group">
+            <label for="exampleFormControlTextarea1">Comentario</label>
+            <textarea name="comentario" placeholder="Ingrese su comentario" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary btn-sm">Enviar</button>
+        <a href="index.php" class="btn btn-primary btn-sm">Cancelar</a>
     </form>
 </div>
--- loggin.php
+++ loggin.php
@@ -10,38 +10,39 @@
     }
 ?>
 
-<div class="container">
-    <div class="col-md-4"></div>
-    <div class="col-md-4">
-        <form class="form-signin" 
+<div class="formAcceso">
+   
+   
+        <form id="formuarioAcceso"
+              class="form-signin" 
               action="controller/VerificarUsuario.php" 
               method="POST">
             <h2 class="form-signin-heading text-center">Acceso</h2>
             <label for="inputNombre" class="sr-only">Email address</label>
-            <input type="text" id="usuario" name="usuario" class="form-control" placeholder="Nombre" required autofocus>
+            <input type="text" id="usuario" name="usuario" class="form-control input-lg" placeholder="Nombre" required autofocus>
             <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="password" name="password" class="form-control" placeholder="Contrase&ntilde;a" required
-               style="    margin-top: 5px;"
-                >
-        <?php
-            echo"<input type='hidden' name='pathAbsolute' value='$pathAbsolute'/> ";
-        ?>
+            <input type="password" id="password" name="password" class="form-control input-lg" placeholder="Contrase&ntilde;a" required
+                   style="    margin-top: 5px;"
+                    />
+            <?php
+                echo"<input type='hidden' name='pathAbsolute' value='$pathAbsolute'/> ";
+            ?>
         
             <div class="checkbox">
               <label>
-                <input type="checkbox" value="remember-me"> Recordar
+                <input type="checkbox" value="remember-me"/> Recordar
               </label>
             </div>
             <a href="recuperaPassword.php">¿Ha olvidado su contrase&ntilde;a?</a>
             <br/><br/>
             <div class="col-md-6">
-                <button class="btn btn-primary btn-block" style="margin-top: 3px;" type="submit">Entrar</button>
+                <button class="btn btn-primary btn-block btn-lg" style="margin-top: 3px;" type="submit">Entrar</button>
             </div>
             <div class="col-md-6">
-                <a href="index.php" class="btn btn-primary btn-block" style="margin-top: 3px;">Cancelar</a>
+                <a href="index.php" class="btn btn-primary btn-block btn-lg" style="margin-top: 3px;">Cancelar</a>
             </div>
         </form>
-    </div>
+    
  </div>
  
  
