8411cc9b00771c88bbfb9461b22cb7b234c0a371
macf3rn4nd0 <macf3rn4nd0@localhost>
2017-10-20 21:17:21 +0200

    MAcbookPro

---
+++ python/servo_up_down.py
@@ -0,0 +1,21 @@
+from sys import argv
+
+script, posicion = argv
+
+import RPi.GPIO as GPIO    #Importamos la libreria RPi.GPIO
+import time                #Importamos time para poder usar time.sleep
+#from sys import argv
+
+import RPi.GPIO as GPIO    #Importamos la libreria RPi.GPIO
+import time                #Importamos time para poder usar time.sleep
+
+GPIO.setmode(GPIO.BCM)   #Ponemos la Raspberry en modo BOARD
+GPIO.setwarnings(False)
+GPIO.setup(20,GPIO.OUT)    #Ponemos el pin 20 como salida
+p = GPIO.PWM(20,50)        #Ponemos el pin 20 en modo PWM y enviamos 50 pulsos $
+p.start(7.5)               #Enviamos un pulso del 7.5% para centrar el servo
+print "Es el parametro: %s"%(posicion)
+p.ChangeDutyCycle(float(posicion))
+time.sleep(0.5)
+p.stop()                      #Detenemos el servo
+GPIO.cleanup()                #Limpiamos los pines GPIO de la Raspberry y cerra$
