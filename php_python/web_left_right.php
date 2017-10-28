5eb3db4680102f3e0a614c968346cc7577c58dde
fernandorosero <tatofarp@hotmail.com>
2017-10-28 17:24:22 +0200

    lenovo i5

--- php_python/web_left_right.php
+++ php_python/web_left_right.php
@@ -1,10 +1,10 @@
 <?php
     $posicion = 20;
-    echo exec("/usr/bin/python2.7 /var/www/html/espiarobot/python/led.py".$posicion.'');
+    echo exec("/usr/bin/python2.7 /var/www/html/espiarobot/python/led.py ".$posicion.'');
     echo ("<H1>pasas:</H1>");
     
     
-    $command = escapeshellcmd('sudo python /var/www/html/espiarobot/python/led.py');
+    $command = escapeshellcmd('sudo python /var/www/html/espiarobot/python/led.py '.$posicion.'');
     $output = exec($command);
     echo "Otra prueba".$output;
     //$posicion = $_POST["valorLR"];
