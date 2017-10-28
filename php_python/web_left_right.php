97a9484f4bb2dd7931f5fe1016a5417b21dcff93
fernandorosero <tatofarp@hotmail.com>
2017-10-28 17:13:17 +0200

    lenovo i5

--- php_python/web_left_right.php
+++ php_python/web_left_right.php
@@ -4,7 +4,7 @@
     
     
     $command = escapeshellcmd('sudo python /var/www/html/espiarobot/python/led.py');
-    $output = shell_exec($command);
+    $output = exec($command);
     echo "Otra prueba".$output;
     //$posicion = $_POST["valorLR"];
     //echo exec('sudo python /var/www/espiarobot/python/servo_leftri_ght.py '.$posicion.'');
