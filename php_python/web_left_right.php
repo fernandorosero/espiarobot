79687f4dbe12b9cc384924c4157388d0d8005cab
fernandorosero <tatofarp@hotmail.com>
2017-10-28 16:45:30 +0200

    Lenovo-Casa

--- php_python/web_left_right.php
+++ php_python/web_left_right.php
@@ -1,5 +1,6 @@
 <?php
-    echo exec('sudo python /var/www/html/espiarobot/python/led.py');
+    echo exec("sudo python /var/www/html/espiarobot/python/led.py");
+    echo ("<H1>pasa:</H1>");
     //$posicion = $_POST["valorLR"];
     //echo exec('sudo python /var/www/espiarobot/python/servo_leftri_ght.py '.$posicion.'');
 ?> 
