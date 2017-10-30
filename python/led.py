fa454c4d5429baac917543e40ef26a35642b7c93
fernandorosero <tatofarp@hotmail.com>
2017-10-30 02:59:49 +0100

    lenovo i5

--- python/led.py
+++ python/led.py
@@ -8,10 +8,10 @@ GPIO.setup(4, GPIO.OUT) #Ponemos el Pin GPIO4 como salida
  
 for i in range(3):
     print"ON"
-    GPIO.output(4, GPIO.HIGH)
+    GPIO.output(21, GPIO.HIGH)
     time.sleep(1)
     print"OFF"
-    GPIO.output(4, GPIO.LOW)
+    GPIO.output(21, GPIO.LOW)
     time.sleep(1)
 print "----DEsde python-posicion: %s"%( posicion)
 GPIO.cleanup() #Reiniciar configuracion pins GPIO
