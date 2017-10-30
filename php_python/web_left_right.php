bb5c31ddab4d57561b08ac03f6842b5e464ac339
fernandorosero <tatofarp@hotmail.com>
2017-10-31 00:52:52 +0100

    lenovo i5

--- php_python/web_left_right.php
+++ php_python/web_left_right.php
@@ -1,4 +1,9 @@
 <?php
+    
+    echo exec('sudo python /var/www/html/espiarobot/python/led.py');
+
+
+    /*
     $posicion = 20;
     echo exec("/usr/bin/python2.7 /var/www/html/espiarobot/python/led.py ".$posicion.'');
     echo ("<H1>pasas:</H1>");
@@ -7,6 +12,8 @@
     $command = escapeshellcmd('sudo python /var/www/html/espiarobot/python/led.py '.$posicion.'');
     $output = exec($command);
     echo "Otra prueba".$output;
+     * 
+     */
     //$posicion = $_POST["valorLR"];
     //echo exec('sudo python /var/www/espiarobot/python/servo_leftri_ght.py '.$posicion.'');
 ?> 
--- python/led.py
+++ python/led.py
@@ -4,14 +4,14 @@ script, posicion = argv
 import RPi.GPIO as GPIO #Libreria Python GPIO
 import time #Libreria Time
 GPIO.setmode(GPIO.BCM) #Establecemos el sisetma de numeracion de pins BCM
-GPIO.setup(4, GPIO.OUT) #Ponemos el Pin GPIO4 como salida
+GPIO.setup(26, GPIO.OUT) #Ponemos el Pin GPIO4 como salida
  
 for i in range(3):
     print"ON"
-    GPIO.output(21, GPIO.HIGH)
+    GPIO.output(26, GPIO.HIGH)
     time.sleep(1)
     print"OFF"
-    GPIO.output(21, GPIO.LOW)
+    GPIO.output(26, GPIO.LOW)
     time.sleep(1)
 print "----DEsde python-posicion: %s"%( posicion)
 GPIO.cleanup() #Reiniciar configuracion pins GPIO
