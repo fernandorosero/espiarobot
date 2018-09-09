a10461d616429a6b81740c1abf155b7f240483d0
frosero1978 <tatofarp@hotmail.com>
2018-09-09 11:18:15 +0200

    Apuntando init servos

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,23 @@
+$(document).ready(function(){
+        inicioServoLeftRight = 7.5;
+        inicioServoUpDown = 7.5;
+	leftRightServo(inicioServoLeftRight);
+	
+});
+function leftRightServo(inicioServoLeftRight){
+    alert ("El valor de leftRight :: " + inicioServoLeftRight);
+    $.ajax({
+        url : 'php_python/web_left_right.php',
+        type : 'POST',
+        data: {arg : inicioServoLeftRight},
+        success: function(response) {
+            alert ("Llamada OK!!!");
+        },
+        error: function() {
+            alert ("No funciona!!!");
+        }
+    });
+}
 function enviarForm(){
     document.loginSend.submit();
 }
